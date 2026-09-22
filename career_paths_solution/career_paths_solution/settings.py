c25c09b261ce3b7b07cb7b6c3fae03228fddded9
Tarun Arumugam <bmh226@txstate.edu>
2026-09-22 11:55:21 -0500

    TFCRT-13 : Create .env file with sensitive data. Pass sensitive data through dotenv library. Create .env.example file for general structure of .env file. Update .gitignore and requirements.txt files.

--- career_paths_solution/career_paths_solution/settings.py
+++ career_paths_solution/career_paths_solution/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qdbh3yip_^(x^3s&gg-8b&^=s%^i%3@)p^))24dem893$b$--1"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
