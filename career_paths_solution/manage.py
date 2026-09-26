3959c18b5e0aa5b8582a72ee42d3e03d6e78474c
tarun-7467 <bmh226@txstate.edu>
2026-09-26 01:58:08 -0500

    Route apps to correct settings

--- career_paths_solution/manage.py
+++ career_paths_solution/manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "career_paths_solution.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "career_paths_solution.settings.development")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
